5ee54f7b9ce5179dc7831b7fa9cb31e1b081820b
Julian.Jimenez.PC_Mesa <ju_lian_17@hotmail.com>
2022-09-10 23:21:38 -0500

    finalizar metodo OnPost() primer insert desde el front

--- AccrontolWeb.App/AccrontolWeb.App.WebApp/Pages/GestionPersonas/Create.cshtml.cs
+++ AccrontolWeb.App/AccrontolWeb.App.WebApp/Pages/GestionPersonas/Create.cshtml.cs
@@ -7,26 +7,52 @@ namespace AccrontolWeb.App.WebApp
 {
     public class CreateModel : PageModel
     {
+        //inastaciar el repositorio para el CRUD
+        private ITrabajadorRepository _TrabajadorRepository = new TrabajadorRepository(new AccrontolWeb.App.Persistencia.AppContext());
+
         public void OnGet()
         {
         }
 
-        public void OnPost(){
+        public void OnPost()
+        {
+            //variables capturadas formulario
             //var = Request.Form [name html]
-            var descripcion = Request.Form["descripcion"];
-            var nombre = Request.Form["nombre"];
-            var usuario = Request.Form["usuario"];
-            var password = Request.Form["password"];
-            var active = 1; //usuario nuevo se registra nuevo
-            var idAreaid = Request.Form["idAreaid"];
-            var idSedeid = Request.Form["idSedeid"];
+            var _identificacion = Request.Form["identificacion"];
+            var _nombre = Request.Form["nombre"];
+            var _usuario = Request.Form["usuario"];
+            var _password = Request.Form["password"];
+            var _active = 1; //usuario nuevo se registra Activo
+            var _Areaid = Request.Form["Areaid"];
+            var _Sedeid = Request.Form["Sedeid"];
 
-            //selector
-            //var SelectorArea = Request.Form["SelectorArea"];
+            //validar datos de las personas con seguridad back
 
-            //validar datos de las personas
+            var crearEmpleado = new Trabajador
+            {
+                //atributoTabla = variableCapturada
+                identificacion = _identificacion,
+                nombre = _nombre,
+                usuario = _usuario,
+                password = _password,
+                active = _active,
+                Areaid = int.Parse(_Areaid),
+                Sedeid = int.Parse(_Sedeid),
+            };
 
+            //realizar el CRUD 
+            //tener presente el valor de retorno          
+            var result = _TrabajadorRepository.Add(crearEmpleado);
 
+            //mostrar al front el resultado de la operacion
+            if (result > 0)
+            {
+                Console.WriteLine("Empleado Registrado");
+            }
+            else
+            {
+                Console.WriteLine("Error al registrar empleado");
+            }
         }
     }
 }
